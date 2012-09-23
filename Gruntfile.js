f3dc535d72f5c51c7c146860c06006b4e488716a
Ben Alman <cowboy@rj3.net>
2012-09-22 20:18:15 -0400

    A few ideas.

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+/*
+ * a new benalman.com, maybe
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ * http://benalman.com/about/license/
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        es5: true,
+        node: true,
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    nodeunit: {
+      files: ['test/**/*.js']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'nodeunit']
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit']);
+
+};
---
+++ test.js
@@ -0,0 +1,59 @@
+/*
+ * a new benalman.com, maybe
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ * http://benalman.com/about/license/
+ */
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var ejs = require('ejs');
+var dateformat = require('dateformat');
+
+var Page = require('./lib/page').Page;
+
+var src = '../benalman.com-content/new';
+var pages = fs.readdirSync(src).map(function(dirname) {
+  // console.log(dirname);
+  var abspath = path.resolve(src, dirname);
+  var page = new Page(abspath);
+  var dest = path.resolve('./build', dirname + '.html');
+
+  var options = Object.create(page);
+  options.filename = './views/page.js';
+  var html = ejs.render('<% include page %>', options);
+  fs.writeFileSync(dest, html);
+
+  return {href: dirname + '.html', meta: page.meta};
+});
+
+var options = {
+  dateformat: dateformat,
+  pages: pages,
+  filename: './views/index.js'
+};
+var html = ejs.render('<% include index %>', options);
+fs.writeFileSync('build/index.html', html);
+
+// // var page = page.create('../benalman.com-content/new/jquery-throttle-debounce');
+// var page = page.create('../benalman.com-content/new/iife');
+// // var page = page.create('./new/');
+
+// console.log(page.index);
+// //console.log(page.documents);
+// console.log(page.meta);
+// console.log('title', page.meta.title);
+// console.log('tags', page.meta.tags);
+// page.meta.foo = 123;
+// console.log('foo', page.meta.foo);
+
+// var options = Object.create(page);
+// options.filename = './views/page.js';
+// var html = ejs.render('<% include page %>', options);
+// // console.log(html);
+
+// fs.writeFileSync('out.html', html);
